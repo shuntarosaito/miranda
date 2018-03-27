74c841f51c803ae79fa3f48f137bb73b39fb89e7
SAITO\shuntaro <shuntaro.saito1321@gmail.com>
2018-03-27 23:29:43 +0900

    bootstrap インストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  def after_sign_in_path_for(resource)
-      user_path(resource)
-  end
 
-  protect_from_forgery with: :exception
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # def after_sign_in_path_for(resource)
+  #     user_path(resource)
+  # end
+
+  # # def after_sign_out_path_for(resource)
+  # #   '/users/sign_in' # サインアウト後のリダイレクト先URL
+  # # end
+
+  # protect_from_forgery with: :exception
 end
