f132029dd60520eba17bcbea5f09ad7155ef7a1d
SAITO\shuntaro <shuntaro.saito1321@gmail.com>
2018-05-14 19:44:14 +0900

    view アップ

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -30,6 +30,11 @@ before_action :authenticate_user!, only: :new
     redirect_to controller: :users, action: :show
   end
 
+  # def graph
+  #   genre = ['','','','']
+  #   aData = [,,,]
+  # end
+
   private
   def create_params
     params.require(:evaluation).permit(:company_id, :salary_rate, :stability_rate, :benefits_rate, :future_rate, :access_rate, :reputation_rate, :self_growth_rate, :human_relationships_rate, :promotion_rate, :working_time_rate, :working_environment_rate, :rewarding_rate, :social_contribution_rate,).merge(user_id: current_user.id)
