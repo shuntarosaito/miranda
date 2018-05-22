bcd9bbc4cc1c14853d73bd61ac1faefab248e10f
SAITO\shuntaro <shuntaro.saito1321@gmail.com>
2018-05-22 20:37:34 +0900

    registration controller create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   # before_action :configure_permitted_parameters, if: :devise_controller?
-
   def after_sign_in_path_for(resource)
        user_path(resource)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: "users/registrations"}
   resources :users, only: :show
   resources :companies, only: [:new, :create, :destroy]
   resources :evaluations, only: [:new, :create, :edit, :update, :destroy]
   resources :display_evaluations, only: [:new, :create, :edit, :update]
   root 'users#show'
-  end
+end
 
